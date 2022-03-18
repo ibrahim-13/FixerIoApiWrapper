5578852bfa29ba73f2b8521454d8c6e200689c87
ibrahim-13 <ib.arshad777@gmail.com>
2022-03-19 00:30:55 +0600

    Added request logging to console

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,12 @@
 ﻿using FixerIoApiWrapper;
 
-var wrapper = new FixerApiWrapper("API_KEY_HERE");
+var wrapper = new FixerApiWrapper("API_KEY_HERE", new()
+{
+#if DEBUG
+    EnableApiResponseLogging = true,
+#else
+    EnableApiResponseLogging = false,
+#endif
+});
 _ = await wrapper.GetSymbolsAsync();
 Console.WriteLine("Hello, World!");
--- FixerIoApiWrapper/Request/LoggedHttpClientHandler.cs
+++ FixerIoApiWrapper/Request/LoggedHttpClientHandler.cs
@@ -12,8 +12,30 @@ internal class LoggedHttpClientHandler : HttpClientHandler
     {
         if (!_isLoggingEnabled) return await base.SendAsync(request, cancellationToken);
 
-        // Do logging here
+        PrintRequestLog(request);
         var response = await base.SendAsync(request, cancellationToken);
+        PrintResponseLog(response);
         return response;
+    }
+
+    private static async void PrintRequestLog(HttpRequestMessage request)
+    {
+        Console.WriteLine("Endpoint: {0}", request.RequestUri?.OriginalString);
+        Console.WriteLine("Request Headers:");
+        Console.WriteLine(request.ToString());
+        Console.WriteLine("Request Body:");
+        if (request.Content != null)
+            Console.WriteLine(await request.Content.ReadAsStringAsync());
+        Console.WriteLine();
+    }
+
+    private static async void PrintResponseLog(HttpResponseMessage response)
+    {
+        Console.WriteLine("Response Headers:");
+        Console.WriteLine(response.ToString());
+        Console.WriteLine("Response Body:");
+        if (response.Content != null)
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        Console.WriteLine();
     }
 }
